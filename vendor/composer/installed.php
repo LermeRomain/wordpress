606a761f1930db44aefad2c7805859c67f0bb37f
giselledunine <55180310+giselledunine@users.noreply.github.com>
2022-05-10 10:05:09 +0200

    commit sophia

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '0d6a61883008da4b32cc36f990d31963479f031d',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '0d6a61883008da4b32cc36f990d31963479f031d',
             'dev_requirement' => false,
         ),
         'brick/math' => array(
